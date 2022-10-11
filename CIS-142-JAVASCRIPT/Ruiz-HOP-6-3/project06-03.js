4de35b6d6f61b5766227f970d08f404c3550d156
Your Name <you@example.com>
2022-10-10 22:10:44 -0500

    added lastest to HOP Projects

---
+++ CIS-142-JAVASCRIPT/Ruiz-HOP-6-3/project06-03.js
@@ -0,0 +1,21 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-03
+
+      Script to complete a form containing billing and shipping address information
+      Author: Sergio Ruiz
+      Date:   October 9, 2022
+
+      Filename: project06-03.js
+*/
+
+      let useShip = document.getElementById("useShip").addEventListener("click", copyShippingToBilling());
+
+      function copyShippingToBilling () 
+      {
+
+
+
+            
+      }
