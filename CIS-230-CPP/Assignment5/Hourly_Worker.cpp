8523674f6209e3b7aef9a6a965154d013d35608a
Serg-Ruiz <sergioruiz55623@gmail.com>
2022-11-07 07:53:28 -0600

    more work done on assignment5

--- CIS-230-CPP/Assignment5/Hourly_Worker.cpp
+++ CIS-230-CPP/Assignment5/Hourly_Worker.cpp
@@ -8,13 +8,82 @@ Hourly_Worker::Hourly_Worker(string _name, int _empId, int _hours, double _Rate)
     setEmpId(_empId);
     setHours(_hours);
     setHourlyRate(_Rate);
-
 }
 
  void Hourly_Worker::setHours(int _hours)
  {
-    hours = _hours
-
-
-    
+    if (_hours > 40)
+    {
+        hours = 40;
+    }
+    else
+    {
+        _hours = hours;
+    }
  }
+
+ int Hourly_Worker::getHours()
+ {
+    return hours + getOTHours();
+ }
+
+void Hourly_Worker::setHourlyRate(double _hourlyRate)
+{
+    hourlyRate = _hourlyRate;
+}
+
+double Hourly_Worker::getHourlyRate()
+{
+    return hourlyRate;
+}
+
+int Hourly_Worker::getOTHours()
+{
+    int otHours;
+
+    if (getHours() > 40)
+    {
+        otHours = getHours() - 40;
+    }
+    else
+    {
+        otHours = 0;
+    }
+    return otHours;
+}
+
+double Hourly_Worker::getOTWages()
+{
+    double otPay = getHourlyRate() * 1.5;
+
+    return otPay * getOTHours();
+}
+
+double Hourly_Worker::getRegularWages()
+{
+    return getHourlyRate() * getHours();
+}
+
+string Employee::to_string()
+{
+
+}
+
+istream& Hourly_Worker::getInput(istream&)
+{
+
+}
+
+double Hourly_Worker::getWages()
+{
+
+}
+
+ostream& operator<<(ostream& my_cout, const Hourly_Worker& param)
+{
+
+}
+istream& operator >> (istream& my_cin, const Hourly_Worker& param)
+{
+
+}
--- CIS-230-CPP/Assignment5/Salaried_Worker.cpp
+++ CIS-230-CPP/Assignment5/Salaried_Worker.cpp
@@ -1,1 +1,44 @@
 #include "Salaried_Worker.h"
+
+Salaried_Worker::Salaried_Worker(string _name, int _id, double _salary)
+{
+    setEmpName(_name);
+    setEmpId(_id);
+    salary = _salary;
+
+}
+
+void Salaried_Worker::setSalary(double _salary)
+{
+    salary = _salary;
+
+}
+
+double Salaried_Worker::getSalary()
+{
+    return salary;
+}
+
+string Salaried_Worker::to_string()
+{
+
+}
+
+istream& Salaried_Worker::getInput(istream&)
+{
+
+}
+
+double Salaried_Worker::getWages()
+{
+
+}
+
+ostream& operator<<(ostream& my_cout, const Salaried_Worker& param)
+{
+
+}
+istream& operator >> (istream& my_cin, const Salaried_Worker& param)
+{
+
+}
