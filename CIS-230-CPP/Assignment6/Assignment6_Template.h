4f2ea300d474aa55ef17bc2010b50f201b1075a2
Serg-Ruiz <sergioruiz55623@gmail.com>
2022-11-24 01:39:11 -0600

    more work done on homework and webproject

---
+++ CIS-230-CPP/Assignment6/Assignment6_Template.h
@@ -0,0 +1,49 @@
+#include <sstream>
+template <class T>
+
+class Assignment6_Template
+{
+private:
+    
+    int size;
+
+public:
+
+    Assignment6_Template();
+    Assignment6_Template(int size);
+    float getAverage();
+    
+    float getSquareRoot();
+
+    string to_string();
+
+        class ArrayWith0Elements
+        {
+            private:
+            string message;
+            public:
+
+            string getMessage()
+            {
+
+            }
+
+        };
+
+        class SquareRootOfNegativeNumberError
+        {
+            private:
+
+            public: 
+
+            string getMessage()
+            {
+
+            }
+
+        };
+
+};
+
+
+
